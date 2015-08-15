f5691d8da3252c0437ae1b0ea88b3d75b5071d1e
wangwendong <wangwendong@wangwendongs-MacBook-Pro.local>
2015-08-15 18:01:15 +0800

    Initial Commit

---
+++ WMCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WMCamera
+//
+//  Created by wangwendong on 15/8/15.
+//  Copyright (c) 2015年 wangwendong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WMCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WMCamera
+//
+//  Created by wangwendong on 15/8/15.
+//  Copyright (c) 2015年 wangwendong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
